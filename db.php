1acc8ff0d49df7d0e7806368ebe01b980acb954d
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-05-22 22:44:57 -0300

    Modificando estrutura para criação de novo dado

--- db.php
+++ db.php
@@ -22,7 +22,7 @@ $schema->create($tabela, function ($table) {
     $table->text('descricao'); // criando uma coluna text
     $table->decimal('preco', 11, 2); // criando uma coluna decimal
     $table->string('fabricante', 60); // criando uma coluna string de tamanho 60
-    $table->date('dt_criacao'); // criando uma coluna tipo date
+    $table->timestamps();
 });
 
 // Preenche a tabela:
@@ -31,12 +31,14 @@ $db->table($tabela)->insert([
     'descricao' => 'Android 11 Samsung One UI 3.0  Tela 6.5  Octa-Core 2.2ghz 5G Câmera 12MP',
     'preco' => 1899.00,
     'fabricante' => 'Samsung',
-    'dt_criacao' => '2021-02-01'
+    'created_at' => '2021-02-01',
+    'updated_at' => '2021-02-01'
 ]);
 $db->table($tabela)->insert([
     'titulo' => 'iPhone X - Cinza Espacial 64gb',
     'descricao' => 'Tela 5.8  IOS 12 4G  Wi-Fi Câmera 12MP - Apple',
     'preco' => 4999.00,
     'fabricante' => 'Apple',
-    'dt_criacao' => '2020-01-10'
+    'created_at' => '2020-01-10',
+    'updated_at' => '2020-01-10'
 ]);
