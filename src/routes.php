0a90e01b2260f046cfaa1ac1b1f3e4b043289340
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-06-15 04:15:46 -0300

    build: :construction: Tratando rotas
    Tratando rotas ou requisições não permitidas ou inexistentes
    
    add map

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,10 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 
 // Routes
 require __DIR__ . '/routes/produtos.php';
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
+
